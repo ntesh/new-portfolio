dd65c3445f320d80b95d87d37c5d357c10380208
Nick Tesh <84792415+ntesh@users.noreply.github.com>
2021-06-27 13:36:17 -0400

    Add files via upload

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,13 @@
-let test = test;
+let gravitySwitch = document.getElementById("gravitySwitch");
+
+gravitySwitch.addEventListener("click", function () {
+  if (gravitySwitch.checked) {
+    document.querySelectorAll(".element").forEach((item) => {
+      item.classList.add("element-float");
+    });
+  } else {
+    document.querySelectorAll(".element").forEach((item) => {
+      item.classList.remove("element-float");
+    });
+  }
+});
